1122e98e06094ac372b3e86310407103f2727d75
noorainnoorani <noorani.noorain@gmail.com>
2025-07-22 23:05:44 -0400

    just trying single dataset to see if T is the limiting factor

--- TNO_tune_lorenz.py
+++ TNO_tune_lorenz.py
@@ -45,7 +45,7 @@ data_grid = [
     # Add more combinations as needed
     # {'T': 5, 'batch_size': 64, 'sample_rate': 0.025},
     # {'T': 5, 'batch_size': 128, 'sample_rate': 0.025},
-    {'T': 20, 'batch_size': 64, 'sample_rate': 0.025},
+    # {'T': 20, 'batch_size': 64, 'sample_rate': 0.025},
     # {'T': 20, 'batch_size': 128, 'sample_rate': 0.025},
     {'T': 50, 'batch_size': 64, 'sample_rate': 0.025},
     # {'T': 50, 'batch_size': 128, 'sample_rate': 0.025}
